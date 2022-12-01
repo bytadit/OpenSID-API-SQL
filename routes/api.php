5b3fa9c4af9ba5255af6be9b1f19580d6dcd94ec
Aditya Bagus Pratama <min.bytadit@gmail.com>
2022-12-01 07:56:52 +0700

    minor

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
+namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -18,24 +18,17 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
-// Route::apiResource('/bloodtypes', App\Http\Controllers\Api\BloodTypeController::class);
-// Route::apiResource('/populations', App\Http\Controllers\Api\PopulationController::class);
-// Route::apiResource('/pemilih', App\Http\Controllers\Api\PemilihController::class);
-// Route::apiResource('/sex', App\Http\Controllers\Api\SexController::class);
-
-
-Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
-Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
+Route::post('/register', RegisterController::class)->name('register');
+Route::post('/login', LoginController::class)->name('login');
+Route::post('/logout', LogoutController::class)->name('logout');
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::group(['middleware' => 'auth:api'], function () {
     Route::apiResources([
-        'bloodtypes' => App\Http\Controllers\Api\BloodTypeController::class,
-        'populations' => App\Http\Controllers\Api\PopulationController::class,
-        'pemilih' => App\Http\Controllers\Api\PemilihController::class,
-        'sex' => App\Http\Controllers\Api\SexController::class,
+        'bloodtypes' => BloodTypeController::class,
+        'populations' => PopulationController::class,
+        'pemilih' => PemilihController::class,
+        'sex' => SexController::class,
     ]);
 });
-Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
